bd7d7157d6fc7de23bbd92891034d0d8caf38d64
Paul Raschke <paul.raschke@hfg-gmuend.de>
2020-12-07 17:42:23 +0100

    Suppress routing to installation view

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import LoginScreen from "./components/LoginScreen";
 
 panel.plugin("thathoff/oauth", {
+  created: () => {
+    // always route back to panel home when trying to access installation view
+    if (panel?.app?.$route?.path === "/installation") {
+      panel.app.$go("/");
+    }
+
+    // prevent routing to the installation view (e.g. like the login view does)
+    panel?.app?.$router?.beforeEach((to, _, next) => {
+      if (to.path !== "/installation") next();
+    });
+  },
   use: [
     {
       install: Vue => {
