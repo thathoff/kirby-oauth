23aa423d61663b68ff1da29de749d0170a1fac2c
Markus Denhoff <markus@denhoff.com>
2021-11-02 22:27:13 +0100

    feat: add support for installation via Oauth in Kirby 3.6
    Currently replacing the whole InstallationView seems to be the only way
    to hook into the installation process.
    
    See #21

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 // add this to support async/await
 import 'regenerator-runtime/runtime'
 import LoginScreen from "./components/LoginScreen";
+import InstallationView from "./components/InstallationView";
 
 panel.plugin("thathoff/oauth", {
-  created: async () => {
-    const settings = await panel.app.$api.get("oauth/settings")
-    if (!settings.enabled) {
-      return
-    }
-
-    // always route back to panel home when trying to access installation view
-    if (panel?.app?.$route?.path === "/installation") {
-      panel.app.$go("/");
-    }
-
-    // prevent routing to the installation view (e.g. like the login view does)
-    panel?.app?.$router?.beforeEach((to, _, next) => {
-      if (to.path !== "/installation") next();
-    });
-  },
-
-  login: LoginScreen
+  login: LoginScreen,
+  components: {
+    'k-installation-view': InstallationView
+  }
 });
